6e9e5c5db2ed26f72b63bfdadeeb90902f46c30d
baizhi958216 <1475289190@qq.com>
2023-04-13 20:11:13 +0800

    vuenote p7
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/VueNotes/index.md
+++ docs/AllDocs/VueNotes/index.md
@@ -3,3 +3,7 @@
 [小满 zs: Vue3 + vite + Ts + pinia + 实战 + 源码 +electron](https://www.bilibili.com/video/BV1dS4y1y7vd)
 
 [模板语法 && vue 指令](./p5)
+
+[虚拟 dom 和 diff 算法](./p6)
+
+[Ref 全家桶 & 源码解析](./p7)
--- docs/AllDocs/VueNotes/index.ts
+++ docs/AllDocs/VueNotes/index.ts
@@ -4,4 +4,12 @@ export default [
     text: "模板语法 && vue指令",
     link: "/AllDocs/VueNotes/p5",
   },
+  {
+    text: "虚拟dom和diff算法",
+    link: "/AllDocs/VueNotes/p6",
+  },
+  {
+    text: "Ref 全家桶 & 源码解析",
+    link: "/AllDocs/VueNotes/p7",
+  },
 ];
