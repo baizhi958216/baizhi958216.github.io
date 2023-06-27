e7c7b102d124a5f4e54292e83df31c5056d1d741
baizhi958216 <1475289190@qq.com>
2023-06-27 10:54:35 +0800

    安卓子系统
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/Windows/index.ts
+++ docs/AllDocs/Windows/index.ts
@@ -4,6 +4,10 @@ export const Windows = (): { text: string; link: string }[] => {
       text: "解决 PowerShell 提示在此系统上禁止运行脚本",
       link: "ps1executionpolicies",
     },
+    {
+      text: "Windows安装安卓子系统",
+      link: "subsystemforandroid",
+    },
   ].map((obj) => {
     return { text: obj.text, link: `/AllDocs/Windows/${obj.link}` };
   });
---
+++ docs/AllDocs/Windows/subsystemforandroid.md
@@ -0,0 +1,15 @@
+# Windows安装安卓子系统(Windows Subsystem for Android)
+
+## 获取安装包
+
+1. 进入https://store.rg-adguard.net/
+2. 选择`Slow`ring
+   ![slow](wsa/slow.png)
+3. 搜索`www.microsoft.com/en-us/p/windows-subsystem-for-android/9p3395vx91nr`下载最新的bundle包
+   ![download](wsa/download.png)
+
+## 安装wsa
+
+双击下载的bundle包
+
+![install](wsa/install.png)
