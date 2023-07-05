a0acda4551d1aef30546921c81d422e1fa0d018c
baizhi958216 <1475289190@qq.com>
2023-07-05 10:06:34 +0800

    rtctime
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/Linux/index.ts
+++ docs/AllDocs/Linux/index.ts
@@ -4,6 +4,7 @@ export const Linux = (): { text: string; link: string }[] => {
     { text: "腾讯云搭建 httpd 服务", link: "tencentcloud" },
     { text: "配置中文输入", link: "archfcitx5" },
     { text: "配置bash环境变量", link: "bashenv" },
+    { text: "Linux与Windows时间不一致研究", link: "rtctime" },
   ].map((obj) => {
     return {
       text: obj.text,
