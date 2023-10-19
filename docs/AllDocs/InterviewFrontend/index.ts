b79c45bda4947771f927429cc4a702cbcd774c22
baizhi958216 <1475289190@qq.com>
2023-10-19 13:15:24 +0800

    搜刮群友
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/InterviewFrontend/index.ts
+++ docs/AllDocs/InterviewFrontend/index.ts
@@ -2,7 +2,8 @@ export const InterviewFrontend = (): { text: string; link: string }[] => {
   return [
     { text: "九悦", link: "jiuyue" },
     { text: "好智", link: "haozhi" },
-    { text: "腾讯凯博", link: "tencentcombo"}
+    { text: "腾讯凯博", link: "tencentcombo"},
+    { text: "搜刮群友的", link: "others" },
   ].map((obj) => {
     return {
       text: obj.text,
