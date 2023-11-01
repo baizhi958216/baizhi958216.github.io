e77076f567209003fb8c1c333132cfc8b4f52699
baizhi958216 <1475289190@qq.com>
2023-11-01 23:20:39 +0800

    doc: 毕业设计
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/KickMe/index.ts
+++ docs/AllDocs/KickMe/index.ts
@@ -1,7 +1,11 @@
 export const kickme = (): { text: string; link: string }[] => {
     return [
         { text: "指指点点", link: "" },
-        { text: "2023DevOps和云计算学习", link: "2023devopsandcloud" }
+        { text: "2023DevOps和云计算学习", link: "2023devopsandcloud" },
+        {
+          text: "毕业设计",
+          link: "graduation_design",
+        }
     ].map((obj) => {
       return {
         text: obj.text,
