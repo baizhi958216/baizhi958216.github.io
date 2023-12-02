3a5ac047035c09a606260cf55ceca7c0d33b5f08
baizhi958216 <1475289190@qq.com>
2023-12-02 23:59:36 +0800

    ncm, sse
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/MyIdeas/index.ts
+++ docs/AllDocs/MyIdeas/index.ts
@@ -2,6 +2,7 @@ export const MyIdeas = (): { text: string; link: string }[] => {
   return [
     { text: "关于Vue的传参", link: "vueprops" },
     { text: "不要让某些网站强奸你(登录复制)", link: "fuckcsdn" },
+    { text: "网易云音乐NCM文件分析", link:"ncmdecrypt"}
   ].map((obj) => {
     return {
       text: obj.text,
