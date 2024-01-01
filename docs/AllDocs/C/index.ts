1c9613cc60ae2c786a797935c22459ec631da1ca
baizhi958216 <1475289190@qq.com>
2024-01-01 22:07:10 +0800

    C: Int 类型溢出问题
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/C/index.ts
+++ docs/AllDocs/C/index.ts
@@ -2,6 +2,7 @@ export const C = (): { text: string; link: string }[] => {
   return [
     { text: "检查字符串", link: "CheckString" },
     { text: "求多项式值", link: "Polynomial" },
+    { text: "int溢出问题", link: "Overflowint" },
   ].map((obj) => {
     return {
       text: obj.text,
