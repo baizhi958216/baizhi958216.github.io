f382f62945e9ba82eb46c508030aee1aca33f5ca
baizhi958216 <1475289190@qq.com>
2024-01-05 19:14:23 +0800

    vue dev tools
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
   ssr: {
     noExternal: ["element-plus"],
   },
+  plugins: [VueDevTools()],
 });
