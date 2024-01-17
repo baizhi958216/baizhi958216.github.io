be6c8cc32e35ba0252ecba042fd5fa583e6b0cb5
baizhi958216 <1475289190@qq.com>
2024-01-17 23:52:54 +0800

    flutter demo flag
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/Flutter/index.ts
+++ docs/AllDocs/Flutter/index.ts
@@ -1,7 +1,8 @@
 export const Flutter = (): { text: string; link: string }[] => {
   return [
     { text: "Flutter", link: "" },
-    { text: "Dart语言基础", link: "dart" }
+    { text: "Dart语言基础", link: "Dart" },
+    { text: "一个商城Demo", link: "FlutterMarket" },
   ].map((obj) => {
     return {
       text: obj.text,
