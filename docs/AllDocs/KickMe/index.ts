20ae9d36a2ddaa9b49d57e00ba165ebce0bb87c8
baizhi958216 <1475289190@qq.com>
2024-01-18 23:07:05 +0800

    nine
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/KickMe/index.ts
+++ docs/AllDocs/KickMe/index.ts
@@ -1,15 +1,19 @@
 export const kickme = (): { text: string; link: string }[] => {
     return [
-        { text: "指指点点", link: "" },
-        { text: "2023DevOps和云计算学习", link: "2023devopsandcloud" },
-        {
-          text: "毕业设计",
-          link: "graduation_design",
-        },
-        {
-          text: "这辈子第一张to签",
-          link: "firsttosticket"
-        }
+      { text: "指指点点", link: "" },
+      { text: "2023DevOps和云计算学习", link: "2023devopsandcloud" },
+      {
+        text: "毕业设计",
+        link: "graduation_design",
+      },
+      {
+        text: "这辈子第一张to签",
+        link: "firsttosticket",
+      },
+      {
+        text: "这辈子第二张to签",
+        link: "nine"
+      },
     ].map((obj) => {
       return {
         text: obj.text,
