9321bccd7c7241c22e825340e64304a133758023
baizhi958216 <1475289190@qq.com>
2024-02-05 23:13:59 +0800

    GNOME的高分辨率屏幕非整数缩放
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/Linux/index.ts
+++ docs/AllDocs/Linux/index.ts
@@ -5,7 +5,8 @@ export const Linux = (): { text: string; link: string }[] => {
     { text: "配置中文输入", link: "archfcitx5" },
     { text: "配置bash环境变量", link: "bashenv" },
     { text: "Linux与Windows时间不一致研究", link: "rtctime" },
-    { text: "让GCC变得可爱", link: "kawaiigcc"}
+    { text: "让GCC变得可爱", link: "kawaiigcc" },
+    { text: "GNOME的屏幕非整数缩放", link: "hidpiscal" },
   ].map((obj) => {
     return {
       text: obj.text,
