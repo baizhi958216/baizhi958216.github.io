bbccb6a74001c7cf7ed55ad945a294af6113b6dd
baizhi958216 <1475289190@qq.com>
2024-08-03 15:25:21 +0800

    peropero
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/InterviewFrontend/index.ts
+++ docs/AllDocs/InterviewFrontend/index.ts
@@ -3,6 +3,7 @@ export const InterviewFrontend = (): { text: string; link: string }[] => {
     { text: "九悦", link: "jiuyue" },
     { text: "好智", link: "haozhi" },
     { text: "腾讯凯博", link: "tencentcombo"},
+    { text: "peropero", link: "peropero" },
     { text: "搜刮群友的", link: "others" },
   ].map((obj) => {
     return {
