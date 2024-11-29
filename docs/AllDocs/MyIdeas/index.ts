f355fd8b8d31817c5b210b79df71cd44883540fb
baizhi958216 <1475289190@qq.com>
2024-11-29 11:07:17 +0800

    55x
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/MyIdeas/index.ts
+++ docs/AllDocs/MyIdeas/index.ts
@@ -2,8 +2,9 @@ export const MyIdeas = (): { text: string; link: string }[] => {
   return [
     { text: "关于Vue的传参", link: "vueprops" },
     { text: "不要让某些网站强奸你(登录复制)", link: "fuckcsdn" },
-    { text: "网易云音乐NCM文件分析", link:"ncmdecrypt"},
+    { text: "网易云音乐NCM文件分析", link: "ncmdecrypt" },
     { text: "玩玩Stable Diffusion", link: "stablediffusion" },
+    { text: "抽象语录？", link: "cnabstract" },
   ].map((obj) => {
     return {
       text: obj.text,
