6d76356d5c2b6d059d8d8087927fb008102bb8d9
baizhi958216 <1475289190@qq.com>
2024-12-12 10:49:18 +0800

    鸡汤
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/AllDocs/Emotion/index.ts
+++ docs/AllDocs/Emotion/index.ts
@@ -1,6 +1,7 @@
 export const Emotion = (): { text: string; link: string }[] => {
     return [
       { text: "不用怀疑，当你遇到了真爱，才会有这5种感觉", link: "truelovesigns" },
+      { text:"女人彻底爱上了你，才会流露出这4个真心表现，不爱装不出来！",link:"lovebehaviors" }
     ].map((obj) => {
       return {
         text: obj.text,
