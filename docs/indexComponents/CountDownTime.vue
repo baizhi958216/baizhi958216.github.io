b8b75ad9f8ad702bc2698aae0e49eecc7e993652
baizhi958216 <1475289190@qq.com>
2024-12-29 17:39:50 +0800

    QAQ
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- docs/indexComponents/CountDownTime.vue
+++ docs/indexComponents/CountDownTime.vue
@@ -32,7 +32,7 @@ const formatTime = (time) => {
   return time.toString().padStart(2, "0");
 };
 
-const targetDate = "2025-03-25";
+const targetDate = "2025-03-26";
 
 setInterval(() => {
   const time = countdownToTime(targetDate);
