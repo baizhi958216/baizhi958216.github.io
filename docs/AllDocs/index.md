68d824183a9f7ccd2c777fe971fbcabba59585bc
baizhi958216 <46777503+baizhi958216@users.noreply.github.com>
2024-12-31 10:37:02 +0800

    QAQ

--- docs/AllDocs/index.md
+++ docs/AllDocs/index.md
@@ -50,7 +50,7 @@ fetch('http://localhost:3001/bangumi_list')
 
 -->
 
-:::warning 倒计时⏱️ 重联将会重置 重置计数: 7
+:::warning 倒计时⏱️ 重联将会重置 重置计数: 8
 <ClientOnly>
   <br/>
     <CountDownTime/>
--- docs/indexComponents/CountDownTime.vue
+++ docs/indexComponents/CountDownTime.vue
@@ -32,7 +32,7 @@ const formatTime = (time) => {
   return time.toString().padStart(2, "0");
 };
 
-const targetDate = "2025-03-27";
+const targetDate = "2025-03-28";
 
 setInterval(() => {
   const time = countdownToTime(targetDate);
